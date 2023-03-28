8fb10b2c5f0b73eafef9867aeb885c1fcea1f2a4
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-03-28 13:56:10 -0300

    arquivos de imagem editados

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -16,32 +16,43 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade <= 2) {
                 //Bebê
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'bebeh.png')
             } else if (idade <= 14) {
                 //Criança
+                img.setAttribute('src', 'criancah.png')
             } else if (idade <= 17) {
                 //Adolescente
+                img.setAttribute('src', 'adolescenteh.png')
             } else if (idade <= 50) {
                 //Adulto
+                img.setAttribute('src', 'adultoh.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'idosoh.png')
             }
 
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade <= 2) {
                 //Bebê
+                img.setAttribute('src', 'bebem.png')
             } else if (idade <= 14) {
                 //Criança
+                img.setAttribute('src', 'criancam.png')
             } else if (idade <= 17) {
                 //Adolescente
+                img.setAttribute('src', 'adolescentem.png')
             } else if (idade <= 50) {
                 //Adulto
+                img.setAttribute('src', 'adultom.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'idosom.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+    
     }
 }
