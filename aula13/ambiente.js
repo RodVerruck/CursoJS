3810ac624f42c2d4283d02929f041215b687054c
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-04-04 14:06:52 -0300

    novos exercícios

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,16 @@
+
+// Essa é um estrutura de repetição com teste lógico no início
+
 var c = 1
-while (c < 10) {
-    console.log('Tudo bem?')
+while (c <= 10) {
+    console.log(`Passo ${c}`)
     c++ // c = c + 1
 }
+
+// Essa é um estrutura de repetição com teste lógico no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 10) 
