2a33f307dda2370d36d7327e94c435b48be1a3b8
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-04-09 08:09:13 -0300

    Update script.js

--- aula14ex/ex016(guanabara)/script.js
+++ aula14ex/ex016(guanabara)/script.js
@@ -5,19 +5,28 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('Erro! faltam dados')
+        res.innerHTML = 'Impossível contar!'
+        // window.alert('Erro! faltam dados')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
+            // Contagem Crescente
             for (let c = 1; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1f449} `
             }
-            res.innerHTML += `\u{1f3c1}`
         } else {
-            for(let c = i; )
+            // Contagem Regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
         }
+        res.innerHTML += `\u{1f3c1}`
     }
 }
