592b6471d65a4964bda7d80574ff1807fc915b3c
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-04-11 22:30:43 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1) //Adiciona o valor 1 depois do último elemento do array
+num.sort() //Organizar o array em ordem crescente
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é o ${num[0]}`)
